f29688c64da2661139b243f9f24f53cdb092e64f
JnathanCarter <jcarter20@stu.jsu.edu>
2023-04-26 22:17:08 -0500

    add fzf to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Update and Install Apt Packages
-LIST_OF_APPS="i3 stow vim tmux ninja-build g++ gettext cmake unzip curl git steghide"
+LIST_OF_APPS="i3 fzf stow vim tmux ninja-build g++ gettext cmake unzip curl git steghide"
 
 sudo apt update
 sudo apt install -y $LIST_OF_APPS
