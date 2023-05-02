4c8e041b6a05cf561000ed96a3435372bb8d8784
JnathanCarter <jcarter20@stu.jsu.edu>
2023-05-02 01:27:23 -0500

    added netrw tree style file listing as default

--- config/.config/nvim/lua/jonathan/init.lua
+++ config/.config/nvim/lua/jonathan/init.lua
@@ -2,4 +2,6 @@ require("jonathan.remap")
 require("jonathan.packer")
 -- Lua initialization file
 vim.cmd [[colorscheme moonfly]]
-vim.cmd [[set relativenumber]]
+vim.cmd [[let g:netrw_liststyle = 3]]
+
+
