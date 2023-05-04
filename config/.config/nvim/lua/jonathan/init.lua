d07b8808d1b5446a3fed43cc57d573d8c48334fc
JnathanCarter <jcarter20@stu.jsu.edu>
2023-05-03 20:06:55 -0500

    change default colorscheme

--- config/.config/nvim/lua/jonathan/init.lua
+++ config/.config/nvim/lua/jonathan/init.lua
@@ -1,7 +1,7 @@
 require("jonathan.remap")
 require("jonathan.packer")
 -- Lua initialization file
-vim.cmd [[colorscheme moonfly]]
+vim.cmd [[colorscheme carbonfox]]
 vim.cmd [[let g:netrw_liststyle = 3]]
 
 
--- config/.config/nvim/lua/jonathan/packer.lua
+++ config/.config/nvim/lua/jonathan/packer.lua
@@ -26,7 +26,8 @@ return require('packer').startup(function(use)
 
 	}
 
-	use { "bluz71/vim-moonfly-colors", as = "moonfly" }
+	--	use { "bluz71/vim-moonfly-colors", as = "moonfly" }
+	use "EdenEast/nightfox.nvim" -- Packer
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('nvim-treesitter/playground')
 	use('ThePrimeagen/harpoon')
